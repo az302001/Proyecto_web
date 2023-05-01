3028cdeb3c7658b48618bfed1430c42d95c012f7
Robert Leon <team@platzi.com>
2023-04-30 19:00:30 -0500

    intento01

--- src/pages/Servicios_Generales/[Detail]/index.jsx
+++ src/pages/Servicios_Generales/[Detail]/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Sgeneral } from '@/Data/Data'
 import styles from './detail.module.css'
-import { useRouter } from 'next/router'
+import  useRouter  from 'next/router'
 import Footer from '@/Components/Footer/Footer'
 import Image from 'next/image'
 import Link from 'next/link'
